7b7cc89d911ff2cfdfdb04d426b28b44aeaf51ff
Jan Boerman <janboerman95@gmail.com>
2022-02-14 23:59:07 +0100

    more descriptive artifact names

--- Common/src/main/java/com/janboerman/starhunt/common/web/EndPoints.java
+++ Common/src/main/java/com/janboerman/starhunt/common/web/EndPoints.java
@@ -4,7 +4,7 @@ public class EndPoints {
 
     private EndPoints() {}
 
-    public static final String ALL_STARS = "/stars";            //POST (GET is more suitable but okhttp3 is preventing us from using that)
+    public static final String ALL_STARS = "/stars";            //POST (GET is more suitable but okhttp3 is disallowing GET requests with a body)
     public static final String SEND_STAR = "/send_star";        //PUT
     public static final String UPDATE_STAR = "/update_star";    //PATCH
     public static final String DELETE_STAR = "/poofed_star";    //DELETE
